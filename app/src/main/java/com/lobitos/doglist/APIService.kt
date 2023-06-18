b857a23eebee58b110674645806e566d43733575
lobito <jgarcia.abx@gmail.com>
2023-06-18 18:32:54 -0500

    dog api basic

---
+++ app/src/main/java/com/lobitos/doglist/APIService.kt
@@ -0,0 +1,13 @@
+package com.lobitos.doglist
+
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Url
+
+interface APIService {
+
+
+    @GET
+    suspend fun getDogsByBreeds(@Url url:String): Response<DogsResponse>
+
+}
